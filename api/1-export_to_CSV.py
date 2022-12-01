198eb38e062cc7bd02b678e2f229d046d5cdee29
hedi <hedihamida98@gmail.com>
2022-12-02 00:36:11 +0100

    write to csv file

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""api"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        id = int(sys.argv[1])
+
+    url = "https://jsonplaceholder.typicode.com/"
+    employee_name = ""
+
+    try:
+        response_users = requests.request("GET", "{}users/{}".format(url, id))
+        response_tasks = requests.request("GET", "{}todos/".format(url))
+        employee_name = response_users.json()["username"]
+
+    except KeyError:
+        pass
+
+    total_number_of_tasks = len(response_tasks.json())
+with open('{}.csv'.format(id), 'w') as f:
+    for i in range(total_number_of_tasks):
+        if response_tasks.json()[i]['userId'] == id:
+
+            f.write(
+                '"{}",'.format(id) +
+                '"{}",'.format(employee_name) +
+                '"{}",'.format(
+                    response_tasks.json()[i]['completed']) +
+                '"{}"'.format(
+                    response_tasks.json()[i]['title'])
+
+
+            )
+            f.write('\n')
