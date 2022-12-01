566ea290b29d10020256efd78bab8968ffbae2e1
hedi <hedihamida98@gmail.com>
2022-12-01 23:38:29 +0100

    api request

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,12 +11,13 @@ if len(sys.argv) == 2:
     id = int(sys.argv[1])
 
 url = "https://jsonplaceholder.typicode.com/"
-
-response_users = requests.request("GET","{}users/{}".format(url,id))
-
-response_tasks = requests.request("GET","{}todos/".format(url))
-
-employee_name = response_users.json()['name']
+employee_name = ""
+try:
+    response_users = requests.request("GET","{}users/{}".format(url,id))
+    response_tasks = requests.request("GET","{}todos/".format(url))
+    employee_name = response_users.json()["name"]
+except KeyError:
+    print("couldn't request data")
 
 total_number_of_tasks = len(response_tasks.json())
 total_number_of_undone_tasks = 0
