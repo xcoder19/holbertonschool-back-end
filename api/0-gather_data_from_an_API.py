bff5bddea98f298c87528db09f01f46d2b03474e
hedi <hedihamida98@gmail.com>
2022-12-01 23:58:21 +0100

    fix formating

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,10 +11,10 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     employee_name = ""
     try:
-        response_users = requests.request("GET","{}users/{}".format(url,id))
-        response_tasks = requests.request("GET","{}todos/".format(url))
+        response_users = requests.request("GET", "{}users/{}".format(url, id))
+        response_tasks = requests.request("GET", "{}todos/".format(url))
         employee_name = response_users.json()["name"]
-    
+
     except KeyError:
         pass
 
@@ -24,18 +24,17 @@ if __name__ == "__main__":
 
     for i in range(total_number_of_tasks):
         if response_tasks.json()[i]['userId'] == id:
-            if response_tasks.json()[i]['completed'] == True:
+            if response_tasks.json()[i]['completed']:
                 total_number_of_done_task += 1
             elif response_tasks.json()[i]['completed'] == False:
-                total_number_of_undone_tasks +=1
+                total_number_of_undone_tasks += 1
 
-    total_number_of_done_or_undone_tasks =  total_number_of_undone_tasks +  total_number_of_done_task          
-    print("Employee {} is done with tasks({}/{}):".format(employee_name,total_number_of_done_task,total_number_of_done_or_undone_tasks))
+    total_number_of_done_or_undone_tasks = total_number_of_undone_tasks + \
+        total_number_of_done_task
+    print("Employee {} is done with tasks({}/{}):".format(employee_name,
+          total_number_of_done_task, total_number_of_done_or_undone_tasks))
     for i in range(total_number_of_tasks):
         if response_tasks.json()[i]['userId'] == id:
-            if response_tasks.json()[i]['completed'] == True:
-                
-                print("\t"+" "+response_tasks.json()[i]['title'])
+            if response_tasks.json()[i]['completed']:
 
-
-
+                print("\t" + " " + response_tasks.json()[i]['title'])
